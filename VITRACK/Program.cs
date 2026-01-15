d3a034dc72bc5b441f2f77d63fbc2e487b3dbe33
Elvin Cavadov <eljav04@Elvins-MacBook-Air.local>
2026-01-15 10:27:52 +0400

    Initial creation

---
+++ VITRACK/Program.cs
@@ -0,0 +1,53 @@
+
+var builder = WebApplication.CreateBuilder(args);
+
+builder.Services.AddControllers();
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen(options =>
+{
+    options.SwaggerDoc("v1", new()
+    {
+        Title = "ViTrackAPI",
+        Version = "v1"
+    });
+
+});
+
+var app = builder.Build();
+
+// pipeline
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI(options =>
+    {
+        options.SwaggerEndpoint("/swagger/v1/swagger.json", "ViTrackAPI v1");
+    });
+}
+
+app.UseHttpsRedirection();
+
+app.MapControllers();
+app.MapGet("/weatherforecast", () =>
+{
+    var summaries = new[]
+    {
+        "Freezing", "Bracing", "Chilly", "Cool", "Mild",
+        "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
+    };
+
+    return Enumerable.Range(1, 5).Select(index =>
+        new WeatherForecast(
+            DateOnly.FromDateTime(DateTime.Now.AddDays(index)),
+            Random.Shared.Next(-20, 55),
+            summaries[Random.Shared.Next(summaries.Length)]
+        ))
+        .ToArray();
+});
+
+app.Run();
+
+record WeatherForecast(DateOnly Date, int TemperatureC, string? Summary)
+{
+    public int TemperatureF => 32 + (int)(TemperatureC / 0.5556);
+}
