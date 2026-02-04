dc91a24018f8bed21d4c679a736d9c72ac990ebd
Elvin Dzhavadov <eljav04@mail.ru>
2026-02-04 14:04:47 +0400

    Added basic log info at program.cs 1.1

--- VITRACK/Program.cs
+++ VITRACK/Program.cs
@@ -47,7 +47,7 @@ builder.Host.UseSerilog((context, configuration) =>
 var app = builder.Build();
 
 app.UseSerilogRequestLogging();
-Log.Information("App is started at " + TimeHelper.GetBakuTime().ToString());
+Log.Warning("App is started at " + TimeHelper.GetBakuTime().ToString());
 
 // Reccomend to disbale after finishing development
 await app.ApplyMigrationsAsync();
@@ -83,7 +83,7 @@ app.UseHttpsRedirection();
 app.MapControllers();
 
 app.MapFallbackToFile("index.html");
-Log.Information("App is runned at " + TimeHelper.GetBakuTime().ToString());
+Log.Warning("App is runned at " + TimeHelper.GetBakuTime().ToString());
 
 app.Run();
 
