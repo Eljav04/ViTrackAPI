f1c0b955c2cc08f8eb404bba93d184b8909a9fb8
Elvin Dzhavadov <eljav04@mail.ru>
2026-02-05 12:35:56 +0400

    Added Statistics repo and contr for get AttRecStatistics

--- VITRACK/Api/Controllers/StatisticsController.cs
+++ VITRACK/Api/Controllers/StatisticsController.cs
@@ -1,6 +1,10 @@
+using System.Security.Claims;
 using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
+using VITRACK.Api.DTOs.Auth;
 using VITRACK.Application.Interfaces;
+using VITRACK.Common.Helpers;
+using VITRACK.Common.Services;
 using VITRACK.Infrastructure.Entities;
 
 namespace VITRACK.Api.Controllers;
@@ -17,4 +21,44 @@ public class StatisticsController : ControllerBase
         _repository = repository;
     }
 
+    [HttpGet("attendace/get-stats-by-employee")]
+    [Authorize(Roles = "Admin,Boss")]
+    public async Task<IActionResult> GetStatisticsByEmployee([FromQuery] string? employeeId = null
+        , [FromQuery] DateOnly? start = null
+        , [FromQuery] DateOnly? end = null
+        , CancellationToken ct = default)
+    {
+        if (start is null)
+            return BadRequest("Başlanğıc tarixi göstərilməyib.");
+        if (end is null)
+            end = TimeHelper.GetBakuDate();
+        if (end < start)
+            return BadRequest("Tarix aralığı yalnışdır. Baş tarix son tarixden böyük ola bilməz.");
+
+        var stats = await _repository.GetStatisticsByParamsAsync(employeeId, start, end, ct);
+
+        return Ok(stats);
+    }
+
+    [HttpGet("attendace/get-my-stats")]
+    [Authorize(Roles = Roles.User)]
+    public async Task<IActionResult> GetMyStatistics([FromQuery] DateOnly? start = null
+        , [FromQuery] DateOnly? end = null
+        , CancellationToken ct = default)
+    {
+        if (start is null)
+            return BadRequest("Başlanğıc tarixi göstərilməyib.");
+        if (end is null)
+            end = TimeHelper.GetBakuDate();
+        if (end < start)
+            return BadRequest("Tarix aralığı yalnışdır. Baş tarix son tarixden böyük ola bilməz.");
+
+        UserInfo? userInfo =
+             JwtService.GetCurrentUserInfo(HttpContext.User.Identity as ClaimsIdentity);
+
+        if (userInfo?.Id is null) return StatusCode(500);
+
+        var stats = await _repository.GetStatisticsByParamsAsync(userInfo.Id, start, end, ct);
+        return Ok(stats);
+    }
 }
